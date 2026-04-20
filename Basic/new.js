94428855872e51cbee8613d261e9bd0b1053524b
ymanish1475-del <ymanish1475@gmail.com>
2026-04-20 10:06:21 +0530

    new

--- Basic/new.js
+++ Basic/new.js
@@ -1,2 +1,14 @@
 console.log("hello world");
 let name="manish yadav";
+console.log(name.length);
+console.log(name[0]);
+// console.log(name[12]); ans = undefined
+l=name.length;
+console.log(name[l-1]);
+console.log(name[name.length-1])
+// console.log(name[length-1]);
+// len=length(name);
+// print(len);
+n="apna collage";
+m=2627;
+console.log(n+m);
