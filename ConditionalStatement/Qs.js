e8bd8da1619e1e62edc10e6e84f1d92d6879af62
ymanish1475-del <ymanish1475@gmail.com>
2026-05-11 21:23:50 +0530

    ass qs

--- ConditionalStatement/Qs.js
+++ ConditionalStatement/Qs.js
@@ -1,26 +1,63 @@
 
+// //QS 1
+// let size = prompt("enter size in 'xl','l','m','s'");
+// let price;
 
-let size = prompt("enter size in 'xl','l','m','s'");
-let price;
+// if(size=="xl"){
 
-if(size=="xl"){
+//     price=250;
 
-    price=250;
+// }else if(size=="l"){
 
-}else if(size=="l"){
+//     price=200;
 
-    price=200;
+// }else if(size=="m"){
 
-}else if(size=="m"){
+//     price=100;
 
-    price=100;
+// }else if(size=="s"){
 
-}else if(size=="s"){
+//     price=50;
 
-    price=50;
+// }else{
+//     alert("please enter right input");
+// }
 
+// console.log(price);
+
+//QS   2
+
+let string=prompt("enter the string");
+
+let s =string.length;
+console.log(string.length);
+
+if(string[0]==="a" && s>3){
+    console.log("GOOD STRING");
 }else{
-    alert("please enter right input");
+    console.log("na")
 }
+// let n=0;
+// let m;
 
-console.log(price);
+// for(let in string){
+//     n++;
+//     m=n
+
+
+
+// }
+// console.log(s)
+// if(string[0]==="a"){
+//     if(s>3){
+//         console.log("GOOD STRING");
+//     }else{
+//         console.log("not good")
+//     }
+
+// }else{
+//     console.log("na")
+// }
+
+
+
