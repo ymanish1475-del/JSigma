50da80132c3b16dde6456705bade27ba9efd5c50
ymanish1475-del <ymanish1475@gmail.com>
2026-05-13 15:21:41 +0530

    pract qs

--- String/PracQs.js
+++ String/PracQs.js
@@ -3,7 +3,7 @@ let msg="help!"
 console.log(msg.toUpperCase().trim());
 
 let name ="ApnaCollege"
-console.log(name.slice(4));
+console.log(name.slice(4).replace("l","t").replace("l","t"));
 let rep=name.slice(4)
-console.log(rep.replace("i","t"));
+console.log(rep.replace("l","t"));
 
