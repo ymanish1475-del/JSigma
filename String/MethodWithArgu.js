ad983275c20d6274e690cf71ad083502280950b4
ymanish1475-del <ymanish1475@gmail.com>
2026-05-13 15:16:48 +0530

    prac qs

---
+++ String/MethodWithArgu.js
@@ -0,0 +1,22 @@
+let str="i am manish kumar . i am founder of litec  . "
+
+// console.log(str.indexOf("of"))
+// console.log(str.indexOf(" "))
+// console.log(str.indexOf("i"))
+
+// console.log(str.toUpperCase().trim())
+
+// console.log(str.slice(4,9))
+// console.log(str.slice(5))
+
+
+// REPLACE METHOD
+
+// console.log(str.slice("i","who"));
+// let rep=str.replace("i","o")
+// console.log(str.replace("i","o"));
+// console.log(rep);
+
+console.log(str.repeat(2));
+
+
---
+++ String/PracQs.js
@@ -0,0 +1,9 @@
+// qs1
+let msg="help!"
+console.log(msg.toUpperCase().trim());
+
+let name ="ApnaCollege"
+console.log(name.slice(4));
+let rep=name.slice(4)
+console.log(rep.replace("i","t"));
+
