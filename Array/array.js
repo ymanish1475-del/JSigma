bee65e7d146fc0326b41d4c8adf717741e9f5e4e
ymanish1475-del <ymanish1475@gmail.com>
2026-05-13 17:24:24 +0530

    arrays

---
+++ Array/array.js
@@ -0,0 +1,19 @@
+
+// let students=["manish","aman","anuj","prabhat"]
+// console.log(students);
+
+// let mark=[89,86,87,83]
+// console.log(mark,mark.length);
+// console.log(students[1][1]);
+
+//arrays are mutable 
+
+let f=["mango","banana","apple"]
+f[0]="banana"
+console.log(f);
+f[8]="apes"
+console.log(f);
+
+
+
+
