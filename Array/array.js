aac1262ba8de59ede553d4b0c4075cb7cda58819
ymanish1475-del <ymanish1475@gmail.com>
2026-05-17 13:34:50 +0530

    splice method

--- Array/array.js
+++ Array/array.js
@@ -37,10 +37,27 @@ let car2=["tata","mahindra","suzuki","toyota","honda","hyundai"]
 
  let car = car1.concat(car2)
  console.log(car)
+
+ //splice
+
+ console.log(car.splice(0,0,"ford","general motor","tesla"));
+ console.log(car);
+ car.splice(5,0,"renault","datson","force")
+console.log(car)
+car.splice(4,1,"G-Wagon")
+console.log(car)
+ 
+ 
+
+//  console.log(car.splice(6,3))   ///delete kar hea 
+
+ //array ko hi change kar deta hea
+//  console.log(car);
+ 
 //  console.log(car.reverse())
- car.slice()                           //slice karta hea indexing ke hisab see
- console.log(car.slice(4,6))       // yanha 4 and 5 bas print hoga 6 print nahi hoga
-console.log(car.slice(0,-3))  
-console.log(car.slice(4))    
-console.log(car.slice())
+//  car.slice()                           //slice karta hea indexing ke hisab see
+//  console.log(car.slice(4,6))       // yanha 4 and 5 bas print hoga 6 print nahi hoga
+// console.log(car.slice(0,-3))  
+// console.log(car.slice(4))    
+// console.log(car.slice())
 
