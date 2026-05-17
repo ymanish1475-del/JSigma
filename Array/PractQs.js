41f2a897b793595a200c41638c8dcb3a8af00cce
ymanish1475-del <ymanish1475@gmail.com>
2026-05-17 14:32:33 +0530

    pravt qs2

--- Array/PractQs.js
+++ Array/PractQs.js
@@ -1,15 +1,25 @@
 // PractQs
 
 let month=["january","july ","march ","august"]
-console.log(month);
-month.shift()
-month.shift()
-month.unshift("june")
-month.unshift("july")
+// console.log(month);
+// month.shift()
+// month.shift()
+// month.unshift("june")
+// month.unshift("july")
 
-console.log(month)
-console.log(month.indexOf("june"))
-console.log(month.indexOf("111"))  // array mai data ka index num search karne ke liye
-console.log(month.includes("july"))  ///arr mai hea ya nahi search karne ke liye
-console.log(month.includes("JULY"))  //FALSE BECAUSE YE CASE SENSITIVE HEA.
+// console.log(month)
+// console.log(month.indexOf("june"))
+// console.log(month.indexOf("111"))  // array mai data ka index num search karne ke liye
+// console.log(month.includes("july"))  ///arr mai hea ya nahi search karne ke liye
+// console.log(month.includes("JULY"))  //FALSE BECAUSE YE CASE SENSITIVE HEA.
+month.splice(0,2,"july ","june")
+console.log(month);
+
+
+// PRACTICE QS2
+
+let language=["python","java","c","c++","c#","html","css","javascript","php","ruby","golang"]
+let rev=language.reverse()
+console.log(rev.indexOf("javascript"))
+
 
--- Array/array.js
+++ Array/array.js
@@ -37,15 +37,16 @@ let car2=["tata","mahindra","suzuki","toyota","honda","hyundai"]
 
  let car = car1.concat(car2)
  console.log(car)
+//  SHORT METHOD
 
  //splice
 
- console.log(car.splice(0,0,"ford","general motor","tesla"));
- console.log(car);
- car.splice(5,0,"renault","datson","force")
-console.log(car)
-car.splice(4,1,"G-Wagon")
-console.log(car)
+//  console.log(car.splice(0,0,"ford","general motor","tesla"));
+//  console.log(car);
+//  car.splice(5,0,"renault","datson","force")
+// console.log(car)
+// car.splice(4,1,"G-Wagon")
+// console.log(car)
  
  
 
@@ -59,5 +60,12 @@ console.log(car)
 //  console.log(car.slice(4,6))       // yanha 4 and 5 bas print hoga 6 print nahi hoga
 // console.log(car.slice(0,-3))  
 // console.log(car.slice(4))    
-// console.log(car.slice())
+// console.log(car.slice()
+
+let arr=[8,9,7,4,78,97,45,9,67,23,94,9]
+console.log(arr);
+
+arr1=arr.sort()
+console.log(arr1)
+console.log(car.sort())
 
