6a46818a08f9d8ef281ac5010f06f619c8ccb8bd
ymanish1475-del <ymanish1475@gmail.com>
2026-05-17 12:30:47 +0530

    concatenation and reverse

--- Array/PractQs.js
+++ Array/PractQs.js
@@ -7,6 +7,9 @@ month.shift()
 month.unshift("june")
 month.unshift("july")
 
-
-
 console.log(month)
+console.log(month.indexOf("june"))
+console.log(month.indexOf("111"))  // array mai data ka index num search karne ke liye
+console.log(month.includes("july"))  ///arr mai hea ya nahi search karne ke liye
+console.log(month.includes("JULY"))  //FALSE BECAUSE YE CASE SENSITIVE HEA.
+
--- Array/array.js
+++ Array/array.js
@@ -8,21 +8,21 @@
 
 //arrays are mutable 
 
-let f=["mango","banana","apple"]
-f[0]="banana"  ///replace
-console.log(f);
-f[8]="apes"
-console.log(f);
-f.push("hii");   //add in last
-console.log(f);
-f.pop();   //del from last
-console.log(f);
-f.shift();  
-    ///sguru sse hata deta hea 
-console.log(f);
-console.log(f.shift())
-f.unshift("papaya");
-console.log(f);
+// let f=["mango","banana","apple"]
+// f[0]="banana"  ///replace
+// console.log(f);
+// f[8]="apes"
+// console.log(f);
+// f.push("hii");   //add in last
+// console.log(f);
+// f.pop();   //del from last
+// console.log(f);
+// f.shift();  
+//     ///sguru sse hata deta hea 
+// console.log(f);
+// console.log(f.shift())
+// f.unshift("papaya");
+// console.log(f);
 
 
 
@@ -30,6 +30,12 @@ console.log(f);
 
 
 // array method
+//CONCATE OF ARRAY S
 
+let car1=["audi","rolls royce","bmw","ferrari","range rover","mecedies benz"]
+let car2=["tata","mahindra","suzuki","toyota","honda","hyundai"]
 
+ let car = car1.concat(car2)
+ console.log(car)
+ console.log(car.reverse())
 
