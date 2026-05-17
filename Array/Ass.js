270e7dec1beb748215801f11cd988d39275b62c0
ymanish1475-del <ymanish1475@gmail.com>
2026-05-17 15:55:49 +0530

    ass qs

---
+++ Array/Ass.js
@@ -0,0 +1,28 @@
+// QS1 
+let arr=[2,4,6,9,3,-3,6,-7]
+console.log(arr.includes(31))
+// console.log(arr)
+// console.log(arr.slice(0,5))
+// console.log(arr.slice(arr.length-3))
+
+
+// for(let i=0;i<=arr.length;i++){
+//     index=arr.indexOf(i)
+//     if(index>=0){
+//         console.log(i)
+//     }
+// }
+
+//qs3 
+let string="manish yadav   "
+console.log(string.trim())
+console.log(string[5])
+
+
+// if(string===""){
+//     console.log("faaaaa")
+// }else{
+//     console.log()
+// }
+
+// /qs4
