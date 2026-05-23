f2691194d285d552aa9db894c0ff4776679d2216
ymanish1475-del <ymanish1475@gmail.com>
2026-05-23 20:42:46 +0530

    loop with arr

---
+++ Loop/LoopWithArr.js
@@ -0,0 +1,5 @@
+let fruit=["mango","banana","apple","apes","orange","litchi"]
+
+for(let i=0;i<fruit.length;i++){
+    console.log(i,fruit[i]);
+}
--- Loop/whileLoop.js
+++ Loop/whileLoop.js
@@ -1,7 +1,34 @@
-let a=1
-let b=10
-while(a<=10){
-    console.log(a*b);
-    a++
+// let a=1
+// let b=10
+// while(a<=10){
+//     console.log(a*b);
+//     a++
 
+// }
+
+let movie="rrr";
+let gauss=prompt("GAUSS MY FAVOR MOVIE : ")
+
+
+while((gauss!=movie)){
+    if(gauss=="quit"){
+        console.log("you quit");
+        break;
+    }
+
+    
+    gauss=prompt("unlucky . this time ")
+    
 }
+
+//BREAK KEYWORD
+
+// let i=1;
+// while(i<=12){
+//     console.log(i);
+//     if(i==10){
+//         break;
+//     }
+    
+//     i++;
+// }
