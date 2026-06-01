8a917c849a806124c7e037d90348dfaae9159934
ymanish1475-del <ymanish1475@gmail.com>
2026-06-01 15:21:46 +0530

    nested loop

--- Loop/LoopWithArr.js
+++ Loop/LoopWithArr.js
@@ -3,3 +3,10 @@ let fruit=["mango","banana","apple","apes","orange","litchi"]
 for(let i=0;i<fruit.length;i++){
     console.log(i,fruit[i]);
 }
+let i=1;
+for(let j=fruit.length-1;j>=0;j--){
+    
+    console.log(i,fruit[j]);
+    i++
+    
+}
---
+++ Loop/NesLoopArr.js
@@ -0,0 +1,10 @@
+let heros=[["Ironman","Spiderman","thor","Black panther"],["Batman","Superman","Flash","Black adam"]]
+
+for(let i=0;i<heros.length;i++){
+    console.log(i);
+    
+    for(let j=0;j<heros[i].length;j++){
+        console.log(heros[i][j]);
+        
+    }
+}
