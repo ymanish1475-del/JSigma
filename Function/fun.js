8ff23c10bea02d5fbcaa8cafa5f18f5bade60a7c
ymanish1475-del <ymanish1475@gmail.com>
2026-07-11 16:23:46 +0530

    fun

--- Function/fun.js
+++ Function/fun.js
@@ -15,4 +15,10 @@ function poem(){
     console.log("up above the world so high,")
     console.log("like a diamond in the sky.")
 }
-poem()
+// poem()
+
+function ludo(){
+    console.log(Math.floor(Math.random()*6)+1)
+}
+
+ludo();
