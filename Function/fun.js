dd68cb719c2d7bfbab7719811fe70eab278265fa
ymanish1475-del <ymanish1475@gmail.com>
2026-07-17 08:34:35 +0530

    fun

--- Function/fun.js
+++ Function/fun.js
@@ -21,4 +21,43 @@ function ludo(){
     console.log(Math.floor(Math.random()*6)+1)
 }
 
-ludo();
+// ludo();
+function name(name){
+    console.log(name);
+}
+manish="kumar";
+
+// name(manish);
+
+function av(a,b,c){
+    
+    console.log("av is "+(a+b+c)/3);
+}
+// let a=Number(prompt("enter a : "))
+// let b=Number(prompt("enter b : "))
+// let c=Number(prompt("enter c : "))
+// av(a,b,c);
+
+
+
+function table(a){
+    for(let i=1;i<=12;i++){
+        let sum=i*a
+
+
+    }
+    // return sum;
+}
+// let a=Number(prompt("enter a : "))
+// table(a)
+// console.log(sum)
+
+function sum(a,b){
+    return (a*b)/2;
+}
+let a=Number(prompt("enter a : "))
+let b=Number(prompt("enter b : "))
+
+console.log(table(a,b))
+
+
